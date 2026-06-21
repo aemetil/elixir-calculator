c2ca82df6a54ca7c4d03ea7c0746d6da69b7071d
aemetil <e.aemetil@gmail.com>
2026-06-21 20:05:27 +0200

    Add CLI script

---
+++ calc.exs
@@ -0,0 +1,14 @@
+# améliorer la CLI
+args =
+  System.argv()
+  |> Enum.reject(fn arg -> arg == "--" end)
+Calculator.main(args)
+
+ #Calculator.main(System.argv())
+
+# pour tester
+# mix run calc.exs -- add 2 3 //Result: 5
+# mix run calc.exs -- add 10 0 //Error: Cannot divide by zero
+
+# Or
+# mix run -e 'Calculator.main(["add", "2", "3"])'
